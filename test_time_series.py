14335605c6c7f31c91fc1d616610ae8a245a578e
Marc Gillioz <marc.gillioz@gmail.com>
2024-01-12 14:11:45 +0100

    implement time series manipulations

--- test_time_series.py
+++ test_time_series.py
@@ -6,12 +6,55 @@ import numpy as np
 @pytest.mark.parametrize("test_input,expected", [
     ([0., 1., None, None, 4., 5., 6., 7., None, 9.], [0., 1., 2., 3., 4., 5., 6., 7., 8., 9.]),
     ([0., 1., 2., None, 4., 5., 6., None, None], [0., 1., 2., 3., 4., 5., 6., 4., 2.]),  # last value missing
-    ([None, None, 2., 3., 4., 5.], [4., 3., 2., 3., 4., 5.]),   # first values missing
-    ([None, 1., 2., 3., None, None], [1.5, 1., 2., 3., 2.5, 2.]),   # both first and last values missing
-    ([0, 1, 2, 3], [0, 1, 2, 3]),   # no missing values
+    ([None, None, 2., 3., 4., 5.], [4., 3., 2., 3., 4., 5.]),  # first values missing
+    ([None, 1., 2., 3., None, None], [1.5, 1., 2., 3., 2.5, 2.]),  # both first and last values missing
+    ([0, 1, 2, 3], [0, 1, 2, 3]),  # no missing values
     (np.array([0., 1., None, None, 4.]), np.array([0., 1., 2., 3., 4.]))
 ])
 def test_interpolate_missing_values(test_input, expected):
     interpolated = ts.interpolate_missing_values(test_input)
     assert len(interpolated) == len(expected)
     assert all([x == y for x, y in zip(interpolated, expected)])
+
+
+@pytest.mark.parametrize("days,steps", [(365, 1), (366, 24), (364, 24 * 4)])
+def test_crop_364(days, steps):
+    series = np.ones(days * steps)
+    result = ts.crop_364(series)
+    expected = np.ones(364 * steps)
+    assert np.all(result == expected)
+
+
+def test_crop_364_fails():
+    series = np.ones(123)
+    with pytest.raises(Exception) as e:
+        result = ts.crop_364(series)
+
+
+def test_find_first_monday():
+    sample_weekday = [1, 2, 3, 2, 1]
+    sample_saturday = [1, 2, 1, 1, 1]
+    sample_sunday = [1, 1, 1, 1, 1]
+    sample_week = (sample_weekday + sample_weekday + sample_saturday + sample_sunday +
+                   sample_weekday + sample_weekday + sample_weekday)
+    time_series = np.repeat(np.array([sample_week]), 52, axis=0).flatten()
+    assert ts.find_first_monday(time_series) == 4
+
+
+def test_find_first_monday_fails():
+    sample_weekday = [1, 2, 3, 2, 1]
+    sample_saturday = [1, 2, 1, 1, 1]
+    sample_sunday = [1, 1, 1, 1, 1]
+    sample_week = (sample_weekday + sample_saturday + sample_weekday + sample_sunday +
+                   sample_weekday + sample_weekday + sample_weekday)  # saturday and sunday do not follow each other
+    time_series = np.repeat(np.array([sample_week]), 52, axis=0).flatten()
+    with pytest.raises(Exception) as e:
+        result = ts.find_first_monday(time_series)
+
+
+@pytest.mark.parametrize("year,expected", [(2024, 0), (2023, 1), (2022, 2), (2021, 3), (2020, 5), (2019, 6)])
+def test_roll_monday(year, expected):
+    time_series = np.array([t for t in range(364 * 24)])
+    result = ts.roll_monday(time_series, year)
+    assert result[0] == 24 * expected
+
--- time_series.py
+++ time_series.py
@@ -1,3 +1,7 @@
+import numpy as np
+import datetime
+
+
 def interpolate_missing_values(time_series: list, verbose: bool = True) -> list:
     """Fill in the missing values (None) in a time series by interpolating between the nearest existing values.
     Wraps around if necessary."""
@@ -24,9 +28,8 @@ def interpolate_missing_values(time_series: list, verbose: bool = True) -> list:
         gaps.append((len(time_series) - gap_length, len(time_series)))
 
     if verbose:
-        print('Interpolating missing values:')
-        print(' -> number of gaps:', len(gaps))
-        print(' -> number of missing values:', sum(gap[1] - gap[0] for gap in gaps))
+        print('Interpolating %d missing values spread over %d gap(s)' %
+              (sum(gap[1] - gap[0] for gap in gaps), len(gaps)))
 
     # fill the gaps with interpolation
     new_time_series = time_series.copy()
@@ -38,3 +41,65 @@ def interpolate_missing_values(time_series: list, verbose: bool = True) -> list:
             new_time_series[t] = (last_value * (gap_end - t) + next_value * (t - gap_begin + 1)) / gap_width
 
     return new_time_series
+
+
+def crop_364(time_series: np.ndarray, verbose: bool = True) -> np.ndarray:
+    """Crops a time series to 364 days (52 weeks * 7 days).
+    Works with different time steps."""
+    verbose_print = print if verbose else lambda *a, **k: None
+    input_length = len(time_series)
+    if input_length % 365 == 0:
+        day_length = input_length // 365
+        verbose_print("Cropping regular year (365 days) to 364 = 52 weeks * 7 days.")
+    elif input_length % 366 == 0:
+        day_length = input_length // 366
+        verbose_print("Cropping leap year (366 days) to 364 = 52 weeks * 7 days.")
+    elif input_length % 364 == 0:
+        day_length = input_length // 364
+    else:
+        raise ValueError("The length of the time series is not compatible with a year (364 to 366 days)")
+    return time_series[0:364 * day_length]
+
+
+def find_first_monday(time_series: np.ndarray) -> int:
+    """Find the position of the first Monday in the series (integer between 0 and 6),
+    based on the assumption that the Saturdays and Sundays are distinguishable from other days of the week."""
+    # change type of time_series to numpy array, in case it was a list
+    time_series = np.array(time_series)
+    # reorganize the time series into weekdays (7 series of length 364 * n_per_day)
+    weekday_series = time_series.reshape((52, 7, -1)).transpose((1, 0, 2)).reshape((7, -1))
+    # compute the covariance between all week days
+    weekday_covariance = np.cov(weekday_series).sum(axis=0)
+    # select the two least correlated days
+    weekend_days, = np.where(np.isin(weekday_covariance, np.sort(weekday_covariance)[0:2]))
+    # pick sunday as the second of the least correlated days, provided that they are in a row (otherwise raise an error)
+    if (weekend_days[0] + 1) % 7 == weekend_days[1]:
+        sunday = weekend_days[1]
+    elif (weekend_days[1] + 1) % 7 == weekend_days[0]:
+        sunday = weekend_days[0]
+    else:
+        raise Exception("The periodicity properties of the time series does not permit to find the first Monday")
+    return (sunday + 1) % 7
+
+
+def roll_monday(time_series: np.ndarray, year: int = None, verbose: bool = True) -> np.ndarray:
+    """Roll the time series to make it start on a Monday.
+    If year is provided, the offset is computed from the calendar.
+    If it is not provided, the first Monday is found from the weekly periodicity."""
+    # change type of time_series to numpy array, in case it was a list
+    time_series = np.array(time_series)
+    # find the number of days until the first monday in the series
+    first_monday = (7 - datetime.date(year, 1, 1).weekday()) % 7 if year is not None \
+        else find_first_monday(time_series)
+    if verbose:
+        print("Rolling the series backward by %d days to make it start on a Monday" % first_monday)
+    steps_per_day = len(time_series) // 364
+    return np.roll(time_series, -steps_per_day * first_monday)
+
+
+def make_364_periodic(time_series: np.ndarray, year: int = None, verbose: bool = True) -> np.ndarray:
+    """Turns a time series into a periodic one, with 52 weeks * 7 days, and starting on a Monday.
+    If year is provided, the offset is computed from the calendar.
+    If it is not provided, the first Monday is found from the weekly periodicity."""
+    cropped_series = crop_364(time_series, verbose=verbose)
+    return roll_monday(cropped_series, year=year, verbose=verbose)
