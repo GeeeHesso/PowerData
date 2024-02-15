261fa812e384a010e171e53c385d958a12fb81fe
Marc Gillioz <marc.gillioz@gmail.com>
2024-02-15 09:44:37 +0100

    add OPF script for PanTaGruEl

---
+++ doc/run_pantagruel.jl
@@ -0,0 +1,17 @@
+# This is a script intended to run the OPF for all time steps of a series
+# with the model PanTaGruEl and the optimizer Ipopt
+
+using PowerModels
+using TemperateOptimalPowerFlow
+using Ipopt
+
+label = length(ARGS) > 0 ? "_$(ARGS[1])" : ""
+
+network = parse_file("pantagruel.json")
+add_line_costs!(network, 2000)
+
+iterate_dc_opf(network,
+	"pantagruel_load_series$(label).csv", 
+	"pantagruel_gen_cost_series$(label).csv",
+	"pantagruel_gen_series$(label).csv", 
+	get_silent_optimizer())
