8cc6714faf40c1089d2b989220061a7e9d947f80
Marc Gillioz <marc.gillioz@gmail.com>
2024-02-23 16:26:21 +0100

    implement ramp constraints

--- doc/run_pantagruel.jl
+++ doc/run_pantagruel.jl
@@ -10,6 +10,8 @@ label = length(ARGS) > 0 ? "_$(ARGS[1])" : ""
 network = parse_file("pantagruel.json")
 add_line_costs!(network, 2000)
 
+assign_ramp_max!(network, 1.0, ["Nuclear", "nuclear", "nuclear_cons"]) # max ramp up/down of 100 MW per hour
+
 iterate_dc_opf(network,
 	"pantagruel_load_series$(label).csv", 
 	"pantagruel_gen_cost_series$(label).csv",
