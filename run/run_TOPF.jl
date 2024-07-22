c555a875fe33908bd0f55e11a23879b5e073e6ef
Marc Gillioz <marc.gillioz@gmail.com>
2024-07-22 14:27:38 +0200

    update TOPF setup and test

--- run/run_TOPF.jl
+++ run/run_TOPF.jl
@@ -1,5 +1,8 @@
+noise_label = length(ARGS) > 0 ? ARGS[1] : 1
+noise_factor = parse(Float64, noise_label)
+
 using TemperateOptimalPowerFlow
 using Gurobi
 
-compute("data/europe_opf_test", "P_result", [52, 168])
-# compute("data/europe_opf_test", "P_result", [13, 28, 24])
+compute("data/TOPF_run", "P_result_$(noise_label)", [52, 168], noise_factor)
+# compute("data/TOPF_run", "P_result_$(noise_label)", [13, 28, 24], noise_factor)
